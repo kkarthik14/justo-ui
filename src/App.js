3490f580b6f85104832ca82b8dee7aa667b57dee
kkarthik14 <83056271+kkarthik14@users.noreply.github.com>
2025-03-13 10:10:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import Login from "./pages/Login";
 import Time from "./pages/Time";
 import OneTimeLink from "./pages/OneTimeLink";
 import KickoutUser from "./pages/KickoutUser";
-// import Home from "./pages/Home";
-// import NotFound from "./pages/NotFound";
 import './App.css';
 
 function App() {
@@ -15,7 +13,6 @@ function App() {
       <header className="App-header">
         <Router>
           <nav>
-            <Link to="/">Home</Link> |
             <Link to="/register">Register</Link> |
             <Link to="/login">Login</Link> |
             <Link to="/time">Get Time</Link> |
@@ -25,13 +22,11 @@ function App() {
 
           </nav>
           <Routes>
-            {/* <Route path="/" element={<Home />} /> */}
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
             <Route path="/time" element={<Time />} />
             <Route path="/one-time-link" element={<OneTimeLink />} />
             <Route path="/kickout-user" element={<KickoutUser />} />
-            {/* <Route path="*" element={<NotFound />} /> */}
           </Routes>
         </Router>
       </header>
